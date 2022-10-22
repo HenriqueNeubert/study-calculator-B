df2b2934415ebfb8900c82dc2481205eadb5ac6c
HenriqueNeubert <neubert.study@gmail.com>
2022-10-22 00:23:29 -0300

    feat: creating validations

--- archive/app.js
+++ archive/app.js
@@ -1,33 +1,39 @@
 const buttons = document.querySelectorAll('.number')
 const operators = document.querySelectorAll('.operation');
 let current = document.querySelector('#current-operations');
+let previous = document.querySelector('#previous-operations');
 
 //DECLARAÇÕES
 
 //FUNÇÕES
 
-function handleCalculateNumber(elem)
-{  
-  current.innerHTML = current.innerHTML + elem;
+function handleCalculateNumber(elem) {
+  if (previous.innerText === '') {
+    current.innerHTML = current.innerHTML + elem;
+  } else {
+    const operator = previous.innerText.slice(-1);
+    const previousUpdate = previous.innerText.slice(0, -1);
+    const result = previous.innerText + operator + current.innerText;
+    console.log(result);
+  }
 }
 
-function handleCalculateOperator(elem)
-{  
-  if(current.innerText){
-
+function handleCalculateOperator(elem) {
+  if (current.innerText != "") {
+    previous.innerText = current.innerText + elem;
+    current.innerText = '';
   }
-  current.innerHTML = current.innerHTML + ' ' + ' ' + elem;
 }
 
 
 //EVENTOS
-buttons.forEach(function (button){
+buttons.forEach(function (button) {
   button.addEventListener('click', () => {
     handleCalculateNumber(button.innerText)
   });
 });
 
-operators.forEach(function (operator){
+operators.forEach(function (operator) {
   operator.addEventListener('click', () => {
     handleCalculateOperator(operator.innerText)
   });
